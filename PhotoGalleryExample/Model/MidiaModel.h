e78f9633c2d4362f2cd02d490e9303e2bc9feaac
Emerson Mendes Filho <emerson.mendesf@gmail.com>
2015-06-01 22:26:35 -0300

    Add model and json

---
+++ PhotoGalleryExample/Model/MidiaModel.h
@@ -0,0 +1,20 @@
+//
+//  MidiaModel.h
+//  PhotoGalleryExample
+//
+//  Created by Emerson Mendes Filho on 01/06/15.
+//  Copyright (c) 2015 Emerson Mendes Filho. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MidiaModel : NSObject
+
+@property(nonatomic, strong) NSString *idMidia;
+@property(nonatomic, strong) NSString *idAlbum;
+@property(nonatomic, strong) NSString *urlThumbMidia;
+@property(nonatomic, strong) NSString *urlMidiaFull;
+
+
+
+@end
---
+++ PhotoGalleryExample/Model/MidiaModel.m
@@ -0,0 +1,14 @@
+//
+//  MidiaModel.m
+//  PhotoGalleryExample
+//
+//  Created by Emerson Mendes Filho on 01/06/15.
+//  Copyright (c) 2015 Emerson Mendes Filho. All rights reserved.
+//
+
+#import "MidiaModel.h"
+
+@implementation MidiaModel
+
+
+@end
